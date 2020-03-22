49bc59a032fb0c5e353c254bcedff2b60201dfe2
Sinthorn Pradutnam <psinthorn@gmail.com>
2020-03-22 11:16:30 +0700

    rename models folder and package to domain

--- domain/contact_model.go
+++ domain/contact_model.go
@@ -1,17 +1,16 @@
-package models
+package domain
 
-type Contact struct{
-	Id 			uint64 	`json:"id"`
-	Title    	string 	`json:"title"`
-	SubTitle 	string 	`json:"sub_title"`
-	Body     	string	`json:"body"`
-	SubBody  	string	`json:"sub_body"`
-	Email    	string	`json:"email"`
-	LineId   	string	`json:"line_id"`
-	Instagram 	string	`json:"instagram"`
-	Facebook 	string	`json:"facebook"`
-	Mobile   	string 	`json:"mobile"`
-	Website  	string	`json:"website"`
-	Status   	bool 	`json:"status"`
+type Contact struct {
+	Id        uint64 `json:"id"`
+	Title     string `json:"title"`
+	SubTitle  string `json:"sub_title"`
+	Body      string `json:"body"`
+	SubBody   string `json:"sub_body"`
+	Email     string `json:"email"`
+	LineId    string `json:"line_id"`
+	Instagram string `json:"instagram"`
+	Facebook  string `json:"facebook"`
+	Mobile    string `json:"mobile"`
+	Website   string `json:"website"`
+	Status    bool   `json:"status"`
 }
-
