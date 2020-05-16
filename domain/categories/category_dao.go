3585396324d120f016526717fcf581f02f42357c
Sinthorn Pradutnam <psinthorn@gmail.com>
2020-05-16 17:43:11 +0700

    Implement read user data from mysql database

---
+++ domain/categories/category_dao.go
@@ -0,0 +1,7 @@
+package categories
+
+//Categories Lists
+// 1. Welcome
+// 2. About
+// 3. Contact
+// 4. Versions
--- domain/contents/content_dao.go
+++ domain/contents/content_dao.go
@@ -1,9 +1,15 @@
 package contents
 
-// var (
-// 	contents = map[int64]*Content{}
-// )
+import utils "github.com/psinthorn/F2Go/utils/errors"
 
-func getContent(id int64) *Content {
+var (
+	contents = map[int64]*Content{}
+)
+
+func Save() *utils.RestErr {
+	return nil
+}
+
+func Get(id int64) *utils.RestErr {
 	return nil
 }
