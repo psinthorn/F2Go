9c07a64b664037062c58bb6a16c74bc786262774
Sinthorn Pradutnam <psinthorn@gmail.com>
2020-05-27 22:15:53 +0700

    Comment DB init()

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -1,8 +1,6 @@
 package users_db
 
 import (
-	"fmt"
-	"log"
 	"os"
 
 	"database/sql"
@@ -26,20 +24,20 @@ var (
 	schema   = os.Getenv(mysql_f2go_schema)
 )
 
-func init() {
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
-		username, password, host, schema,
-	)
-	//fmt.Println(dataSourceName)
-	var err error
-	Client, err = sql.Open("mysql", dataSourceName)
-	if err != nil {
-		panic(err)
-	}
-	if err = Client.Ping(); err != nil {
-		panic(err)
-	}
-	log.Println("MySQL is successfully figured")
+// func init() {
+// 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
+// 		username, password, host, schema,
+// 	)
+// 	//fmt.Println(dataSourceName)
+// 	var err error
+// 	Client, err = sql.Open("mysql", dataSourceName)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	if err = Client.Ping(); err != nil {
+// 		panic(err)
+// 	}
+// 	log.Println("MySQL is successfully figured")
 
-	return
-}
+// 	return
+// }
