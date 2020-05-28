9f9a1dcbeb661aa59eba5462c4097648096fceb3
Sinthorn Pradutnam <psinthorn@gmail.com>
2020-05-29 05:29:18 +0700

    Implement encryp password

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -1,6 +1,8 @@
 package users_db
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"database/sql"
@@ -24,20 +26,20 @@ var (
 	schema   = os.Getenv(mysql_f2go_schema)
 )
 
-// func init() {
-// 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
-// 		username, password, host, schema,
-// 	)
-// 	//fmt.Println(dataSourceName)
-// 	var err error
-// 	Client, err = sql.Open("mysql", dataSourceName)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	if err = Client.Ping(); err != nil {
-// 		panic(err)
-// 	}
-// 	log.Println("MySQL is successfully figured")
+func init() {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
+		username, password, host, schema,
+	)
+	//fmt.Println(dataSourceName)
+	var err error
+	Client, err = sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+	if err = Client.Ping(); err != nil {
+		panic(err)
+	}
+	log.Println("MySQL is successfully figured")
 
-// 	return
-// }
+	return
+}
